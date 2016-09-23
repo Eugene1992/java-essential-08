0c39da7965e175786c0a13c1d92bb57d8676e99e
mankunianeth <deyneko55@gmail.com>
2016-09-23 09:27:46 +0300

    Test branch.

---
+++ src/main/java/com/cbs/edu/lists/Test.java
@@ -0,0 +1,16 @@
+package com.cbs.edu.lists;
+
+import java.util.ArrayList;
+
+/**
+ * Created by evgeniy on 23/09/16.
+ */
+public class Test {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        list.add("S");
+        list.add("D");
+        list.add("D");
+        list.add("D");
+    }
+}
