41b128ed5778f1af95edab8fe1c739f700cb9bf3
Gleb73 <Tarasevic.hleb@gmail.com>
2016-09-28 21:13:51 +0300

    ArrayList and LinkendList

--- gleb/src/main/java/hw1/ArrayLinkedList.java
+++ gleb/src/main/java/hw1/ArrayLinkedList.java
@@ -7,52 +7,58 @@ import java.util.List;
 /**
  * Created by Gleb on 25.09.2016.
  */
-public class Array_list {
+public class ArrayLinkedList {
     public static void main(String[] arg) {
+        int a = 10, b = 10, c = 300;
+        arrayLinkedList(a, b, c);
+    }
+        static void arrayLinkedList(int a, int b, int c){
         List<String> arrayList = new ArrayList<>();
         List<String> linkedList = new LinkedList<>();
-
-        for (int i = 0; i < 10000; i++) {
+        for (int i = 0; i < 10_000; i++) {
             arrayList.add("Hello world!");
             linkedList.add("Hello world!");
         }
-
         long startGetAl = System.currentTimeMillis();
-        for (int i = 0; i < 10000; i++){
-            arrayList.get(500);
+        for (int i = 0; i < 10_000; i++){
+            arrayList.get(a);
         }
         long endGetAL = System.currentTimeMillis();
         System.out.println("ArrayList .get() - " + (endGetAL - startGetAl));
 
         long startGetAlL = System.currentTimeMillis();
-        for (int i = 0; i < 10000; i++){
-            linkedList.get(500);
+        for (int i = 0; i < 10_000; i++){
+            linkedList.get(b);
         }
         long endGetALL = System.currentTimeMillis();
-        System.out.println("ArrayList .get() - " + (endGetALL - startGetAlL));
+        System.out.println("LinkedList .get() - " + (endGetALL - startGetAlL));
 
         long startSetAl = System.currentTimeMillis();
-        for (int i = 0; i < 10000; i++){
-            arrayList.set(500, "Hello");
+        for (int i = 0; i < 10_000; i++){
+            arrayList.set(a, "Hello");
         }
         long endSetAL = System.currentTimeMillis();
         System.out.println("ArrayList .set() - " + (endSetAL - startSetAl));
 
         long startSetAlL = System.currentTimeMillis();
-        for (int i = 0; i < 10000; i++){
-            linkedList.set(500, "Hello");
+        for (int i = 0; i < 10_000; i++){
+            linkedList.set(b, "Hello");
         }
         long endSetALL = System.currentTimeMillis();
-        System.out.println("ArrayList .set() - " + (endSetALL - startSetAlL));
+        System.out.println("LinkedList .set() - " + (endSetALL - startSetAlL));
 
         long startRemoveAl = System.currentTimeMillis();
-            arrayList.remove(400);
+            for (int i = 0; i < c; i++) {
+                arrayList.remove(a);
+            }
         long endRemoveAL = System.currentTimeMillis();
         System.out.println("ArrayList .rem() - " + (endRemoveAL - startRemoveAl));
 
         long startRemoveAlL = System.currentTimeMillis();
-            linkedList.remove(400);
+            for (int i = 0; i < c; i++) {
+                linkedList.remove(b);
+            }
         long endRemoveALL = System.currentTimeMillis();
-        System.out.println("ArrayList .rem() - " + (endRemoveALL - startRemoveAlL));
+        System.out.println("LinkedList .rem() - " + (endRemoveALL - startRemoveAlL));
     }
 }
